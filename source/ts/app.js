3fd0a9ea93eee56011e145fdd3f69d563ac6660f
liviu.ignat <liviu.ignat@check24.de>
2015-07-28 13:05:25 +0200

    Remove js file output

--- source/ts/app.js
+++
@@ -1,2 +0,0 @@
-var a = 1;
-window.document.write('Hello Wrold');
--- test/main.test.js
+++
@@ -1,6 +0,0 @@
-/// <reference path="./interfaces.d.ts"/>
-describe('when started', function () {
-    it('should be true', function () {
-        expect(true).to.equal(true);
-    });
-});
